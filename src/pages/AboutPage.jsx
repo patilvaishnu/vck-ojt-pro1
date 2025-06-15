3466c909b98db6b2a1ff1c801a51059dc8ae96da
vaishnavi patil <vaishnup2602@gmail.com>
2025-06-15 16:41:07 +0530

    first commit

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,12 @@
+import Header from "../conponentes/Header";
+
+const AboutPage = () => {
+ return(
+    <div>
+        <Header/>
+            <h1>This is about page </h1>
+    </div>
+ )
+}
+
+export default AboutPage;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import Header from "../conponentes/Header";
+
+const HomePage = () => {
+ return(
+    <div>
+        <Header/>
+        <h1>VCK OJT</h1>
+        <p>This is my first project</p>
+    </div>
+ )
+}
+
+export default HomePage;
